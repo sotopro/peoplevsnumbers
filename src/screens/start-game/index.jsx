07072d17c0d2a063d2dfb2636a96440b8d307ea7
Daniel Soto <dasg.1993@gmail.com>
2023-04-26 20:27:04 -0500

    [feature] - add props to text input and implement keyboard dismiss in start screen

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,23 +1,41 @@
-import { Button, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import { styles } from './styles';
 import { Card, Header } from '../../components/index';
 import { theme } from '../../constants';
 
 const StartGame = () => {
+  const [numberOption, setNumberOption] = useState('');
+
+  const onHandlerChangeText = (text) => {
+    setNumberOption(text.replace(/[^0-9]/g, ''));
+  };
   return (
-    <View style={styles.container}>
-      <Header title="Welcome" />
-      <Text style={styles.title}>Start Game</Text>
-      <Card style={styles.inputContainer}>
-        <Text style={styles.label}>Write a number</Text>
-        <TextInput placeholder="0" style={styles.input} />
-        <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} color={theme.colors.secondary} />
-          <Button title="Confirm" onPress={() => {}} color={theme.colors.primary} />
-        </View>
-      </Card>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header title="Welcome" />
+        <Text style={styles.title}>Start Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text style={styles.label}>Write a number</Text>
+          <TextInput
+            placeholder="0"
+            style={styles.input}
+            keyboardType="number-pad"
+            maxLength={2}
+            autoCapitalize="none"
+            autoCorrect={false}
+            blurOnSubmit
+            onChangeText={onHandlerChangeText}
+            value={numberOption}
+          />
+          <View style={styles.buttonContainer}>
+            <Button title="Reset" onPress={() => {}} color={theme.colors.secondary} />
+            <Button title="Confirm" onPress={() => {}} color={theme.colors.primary} />
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
