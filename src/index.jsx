b2e1f39e74c72891bb93242386fdcf9f1b62ed13
Daniel Soto <dasg.1993@gmail.com>
2023-04-26 19:08:42 -0500

    [feature] - setup and cofig eslint prettier and split app js and create folder structure

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola.</Text>
+    </View>
+  );
+}
